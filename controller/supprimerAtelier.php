ec85712ab32eb2d30d82497a35028ec6ac1bd45c
adrien_lamoureux <vergil1534@gmail.com>
2014-09-25 16:25:31 +0200

    supprimerAtelier : commentaires

--- controller/supprimerAtelier.php
+++ controller/supprimerAtelier.php
@@ -1,9 +1,15 @@
 <?php
-//Connection database
-$id_conn = mysqli_connect('localhost', 'root', '','cnrs')or die('impossible de se connecter');
-mysqli_select_db($id_conn,'cnrs')or die("connection a la base impossible");
-$var=$_GET['titre'];
-$request = "delete from atelier where atelier.titre ='$var' ";
-$res = mysqli_query($id_conn,$request);
-header("location: ../vue/listeAteliers.php"); 	
+	//Database connection
+	$id_conn = mysqli_connect('localhost', 'root', '','cnrs')or die('impossible de se connecter');
+
+	//Select database
+	mysqli_select_db($id_conn,'cnrs')or die("connection a la base impossible");
+
+	//get by title
+	$title=$_GET['titre'];
+	$request = "delete from atelier where atelier.titre ='$title' ";
+	$res = mysqli_query($id_conn,$request);
+
+	//Come back to the listeAteliers after remove
+	header("location: ../vue/listeAteliers.php"); 	
 ?>
