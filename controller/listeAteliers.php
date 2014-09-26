cf33c6c1b1049ca1d8784565ff36fd174d88ff85
adrien_lamoureux <vergil1534@gmail.com>
2014-09-26 11:46:15 +0200

    listeAteliers
    Ajout d'un champ lieu en sortie de la requête SQL

--- controller/listeAteliers.php
+++ controller/listeAteliers.php
@@ -6,16 +6,17 @@
 	mysqli_select_db($id_conn,'cnrs')or die("connection a la base impossible");
 
 	//Select data from "atelier"
-	$request="select titre, thematique, type from atelier ";
+	$request="select titre, thematique, type, Lieu from atelier ";
 	$res = mysqli_query($id_conn,$request);
 
 	//Routine to load data and print as a simple list
 	while($ligne = mysqli_fetch_row($res)){
-		echo "<tr style='color:blue;text-align:center'>\n
+		echo "<tr>\n
 	       <td><i/><b/>$ligne[0]</td>
 		   <td><i/><b/>$ligne[1]</td>
-		   <td><i/><b/>$ligne[2]</td> 
-			<td><img src=../image/mod.png><a href = '../controller/supprimerAtelier.php?titre=$ligne[0]' /a><img src=../image/sup.png></td>";
+		   <td><i/><b/>$ligne[2]</td>
+		   <td><i/><b/>$ligne[3]</td>
+		   <td><img src=../image/mod.png><a href = '../controller/supprimerAtelier.php?titre=$ligne[0]' /a><img src=../image/sup.png></td>";
         echo " </tr>\n";
 	}
 ?>
