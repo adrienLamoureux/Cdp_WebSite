083df86a35275db201fd7824612e80bdc0b1de79
adrien_lamoureux <vergil1534@gmail.com>
2014-09-26 22:29:35 +0200

    listeAteliers
    Création d"une classe + fonctions pour Test E2E

--- controller/listeAteliers.php
+++ controller/listeAteliers.php
@@ -1,22 +1,46 @@
 <?php
-	//Database connection
-	$id_conn = mysqli_connect('localhost', 'root', '','cnrs')or die('impossible de se connecter');
+class TableAteliers
+{
+	private $_id_conn;
 
-	//Select database
-	mysqli_select_db($id_conn,'cnrs')or die("connection a la base impossible");
-
-	//Select data from "atelier"
-	$request="select titre, thematique, type, Lieu from atelier ";
-	$res = mysqli_query($id_conn,$request);
-
-	//Routine to load data and print as a simple list
-	while($ligne = mysqli_fetch_row($res)){
-		echo "<tr>\n
-	       <td><i/><b/><a href = '../controller/afficherAtelier.php?titre=$ligne[0]'>$ligne[0]</a></td>
-		   <td><i/><b/>$ligne[1]</td>
-		   <td><i/><b/>$ligne[2]</td>
-		   <td><i/><b/>$ligne[3]</td>
-		   <td><a href = '../controller/modifierAtelier.php?titre=$ligne[0]'/a><img src=../image/mod.png><a href = '../controller/supprimerAtelier.php?titre=$ligne[0]'/a><img src=../image/sup.png></td>";
-        echo " </tr>\n";
+	public function initiate_connection(){
+		//Database connection
+		$this->_id_conn = mysqli_connect('localhost', 'root', '','cnrs')or die('impossible de se connecter');
+		//Select database
+		mysqli_select_db($this->_id_conn,'cnrs')or die("connection a la base impossible");
 	}
+	
+	public function get_ateliers(){
+		//Select data from "atelier"
+		$request="select titre, thematique, type, Lieu from atelier ";
+		$res = mysqli_query($this->_id_conn,$request);
+		return $res;
+	}
+	
+	public function build_table($res){
+		//Routine to load data and print as a simple list
+		while($ligne = mysqli_fetch_row($res)){
+			echo "<tr>\n";
+			$this->build_lines($ligne);
+			echo " </tr>\n";
+		}
+	}
+	
+	protected function build_lines($ligne){
+		echo "<td><i/><b/><a href = '../controller/afficherAtelier.php?titre=$ligne[0]'>$ligne[0]</a></td>";
+		for($i=1;$i<count($ligne);$i++){
+			echo "<td><i/><b/>$ligne[$i]</td>";
+		}
+		echo "<td><a href = '../controller/modifierAtelier.php?titre=$ligne[0]'/a><img src=../image/mod.png><a href = '../controller/supprimerAtelier.php?titre=$ligne[0]'/a><img src=../image/sup.png></td>";
+	}
+
+	//TODO
+	// TEST FUNCTION for E2E
+	// TEST FUNCTION for BDD
+		
+}
+$t_ateliers = new TableAteliers(); 
+$t_ateliers->initiate_connection();
+$ateliers = $t_ateliers->get_ateliers();
+$t_ateliers->build_table($ateliers);
 ?>
